2f68fec083bf868b8750a71ff1e7abca4fb46002
nayem-codes <nayem83346@gmail.com>
2026-05-04 20:17:20 +0600

    added all books section

---
+++ src/app/all-photo/page.jsx
@@ -0,0 +1,18 @@
+import PhotoCard from '@/components/PhotoCard';
+import React from 'react';
+
+const AllPhotosPage = () => {
+    const res = await fetch('https://assignment08-sable.vercel.app/data.json')
+    const photos = await res.json()
+    return (
+        <div>
+            <h1 className='text-2xl font-bold m-4'>All Books</h1>
+
+            <div className='grid grid-cols-4 gap-4'>
+                {photos.map(photo => <PhotoCard key={photo.id} photo={photo}/>)}
+            </div>
+        </div>
+    );
+};
+
+export default AllPhotosPage;
