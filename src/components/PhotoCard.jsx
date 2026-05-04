ffe9a0592b0521790a841cb70f83c794a79787e6
nayem-codes <nayem83346@gmail.com>
2026-05-04 19:34:49 +0600

    added top books card

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,17 +1,17 @@
-import { Card, Chip, Separator } from '@heroui/react';
+import { Button, Card, Chip, Separator } from '@heroui/react';
 import Image from 'next/image';
 import React from 'react';
 
-const PhotoCard = ({photo}) => {
+const PhotoCard = ({ photo }) => {
     return (
         <Card className='border'>
             <div className='relative w-full aspect-square'>
                 <Image
-                src={photo.image_url}
-                fill
-                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                alt={photo.title}
-                className='object-cover rounded-xl'
+                    src={photo.image_url}
+                    fill
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    alt={photo.title}
+                    className='object-cover rounded-xl'
                 />
 
                 <Chip className='absolute right-2 top-2'>{photo.category}</Chip>
@@ -21,15 +21,19 @@ const PhotoCard = ({photo}) => {
                 <h2>{photo.title}</h2>
             </div>
 
-            <div>
-                <p>{photo.available_quantity}</p>
+            <div className='flex gap-4'>
+                <div>
+                    <p>{photo.available_quantity}</p>
+                </div>
+
+                <Separator orientation='vertical' />
+
+                <div>
+                    <p>{photo.author}</p>
+                </div>
             </div>
 
-            <Separator orientation='vertical'/>
-
-            <div>
-                <p>{photo.author}</p>
-            </div>
+            <Button variant='outline' className={"w-full"}>View</Button>
         </Card>
     );
 };
